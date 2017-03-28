8d30716da0fea3644663ca1ac6788592eb3d2fa7
lgstuehrmann <lgstuehrmann18@gmail.com>
2017-03-28 18:33:14 -0400

    Worked on controllers for SearchMenu and SearchResults.

--- SearchMenu/src/sample/Controller.java
+++ SearchMenu/src/sample/Controller.java
@@ -1,4 +1,24 @@
 package sample;
-/*comment*/
+
+import javafx.event.EventHandler;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
+import java.awt.event.ActionEvent;
+import java.util.ResourceBundle;
+import javafx.scene.control.CheckBox;
+
 public class Controller {
+    @FXML
+    Button: Back;
+    @FXML
+    Button: Search;
+    @FXML
+    CheckBox: IsWine;
+    @FXML
+    CheckBox: IsBeer;
+    @FXML
+    CheckBox: IsOther;
+    @FXML
+    TextField: Tags;
 }
--- SearchResults/src/sample/Controller.java
+++ SearchResults/src/sample/Controller.java
@@ -1,4 +1,23 @@
 package sample;
 
+import javafx.event.EventHandler;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.TableColumn;
+
 public class Controller {
+    @FXML
+    TableColumn: IDno;
+    @FXML
+    TableColumn: Name;
+    @FXML
+    TableColumn: Company;
+    @FXML
+    TableColumn: Type;
+    @FXML
+    TableColumn: Date;
+    @FXML
+    Button: downRes;
+    @FXML
+    Button: newSearch;
 }
