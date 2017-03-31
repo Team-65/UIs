e65a946126d386eeced560eaaae708c5028b8fa0
peternolan <pnolan@wpi.edu>
2017-03-31 18:16:53 -0400

    We found errors in the Controller.java for Search Results. The error appeared to be that the controller assets were not properly structured. We fixed this error by getting rid of the semicolons that were in between the type and the name, i.e.:   TableColumn: IDno;   became   TableColumn IDno;
      This cange was made at 6:10 pm, Mar 31.

--- SearchResults/src/sample/Controller.java
+++ SearchResults/src/sample/Controller.java
@@ -6,18 +6,11 @@ import javafx.scene.control.Button;
 import javafx.scene.control.TableColumn;
 
 public class Controller {
-    @FXML
-    TableColumn: IDno;
-    @FXML
-    TableColumn: Name;
-    @FXML
-    TableColumn: Company;
-    @FXML
-    TableColumn: Type;
-    @FXML
-    TableColumn: Date;
-    @FXML
-    Button: downRes;
-    @FXML
-    Button: newSearch;
+    @FXML TableColumn IDno;
+    @FXML TableColumn Name;
+    @FXML TableColumn Company;
+    @FXML TableColumn Type;
+    @FXML TableColumn Date;
+    @FXML Button downRes;
+    @FXML Button newSearch;
 }
